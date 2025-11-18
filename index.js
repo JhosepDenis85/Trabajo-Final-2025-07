fe9d599385a6f5fe2a068e615eedad5ac10d607a
Jhosep Denis Cueva Milla <jhoden2005@gmail.com>
2025-11-17 20:15:17 -0500

    Trabajo Final v2

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ require('./src/routes/auth.routes')(app);
 require('./src/routes/user.routes')(app);
 
 app.get('/',(req,res)=>{
-    res.send('Hola Jhosep');
+    res.send('Hola Jhosep - Trabajo Final - IDAT 2025');
 })
 
 const db = require('./src/models');
